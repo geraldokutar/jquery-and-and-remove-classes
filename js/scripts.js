109e8d0ecc4b22e688899b6e4400b222d5eba3b7
geraldokutar <geraldpilk@gmail.com>
2018-07-25 13:10:27 +0300

    Adding javascript rule so that when we click a button it add the class to body

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $("button#green").click(function(){
-    $("body").addClass("green-background");
+    $("body").addClass("green-background")
   });
+  $("button#yellow").click(function(){
+    $("body").addClass("yellow-background")
+  });
+  $("button#red").click(function(){
+    $("body").addClass("red-background")
+  })
 });
